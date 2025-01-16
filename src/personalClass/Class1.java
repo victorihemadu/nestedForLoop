e36eb5d78e2a258b82791c89017c4866144c762c
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2025-01-16 05:05:57 +0100

    Nested For Loop

---
+++ src/personalClass/Class1.java
@@ -0,0 +1,86 @@
+package personalClass;
+
+import java.sql.SQLOutput;
+
+import java.util.Scanner;
+
+import static java.awt.SystemColor.info;
+
+
+public class Class1 {
+
+    public void anyMethod(){
+        System.out.println("First method");
+    }
+
+    public String anyMethod1() {
+        return "Benjamin";
+    }
+
+    public void anyMethod2() {
+        System.out.println("Tester");
+    }
+
+    public int anyMethod3(){
+        return 27;
+    }
+
+    public static void main(String[] args) {
+        /*
+        String names = "John";
+        System.out.println(names);
+        System.out.println(43&19);
+
+        //use scanner to collect info from a user
+
+        String name = "Paul";
+
+        String name1 = new String("King");
+
+        Scanner info = new Scanner(System.in);
+
+        System.out.print("please enter your name:");
+
+        String name2  = info.nextLine();
+
+        System.out.print("please enter your age:");
+
+        int age = info.nextInt();
+
+        System.out.println("my name is " + name + " and I am " + age + " years old");
+
+        */
+
+        /// /// Java Day 2 14/01/2025
+
+        /*
+        Scanner info = new Scanner(System.in); //used to collect input from a user.
+        System.out.println("please enter your name:");
+
+        String name = info.nextLine();
+
+        System.out.print("please enter your age:");
+
+        int age = info.nextInt();
+
+        //System.out.println("my name is " + name + " and I am " + age + " years old");
+
+        System.out.println(STR."my name is \{name} and I am \{age} years old");
+        */
+        /*
+        for(int i=1; i<5; i++) {
+            for (int j=0; j<4;  j++) {
+                System.out.print("# ");
+            }
+            System.out.println(" ");
+        }
+        */
+
+        for(int i=0; i<5; i++) {
+             /*for (int j=i; j<4;  j++) {
+                System.out.print("# ");
+            }*/
+            System.out.println(" ");
+        }
+    }
+}
