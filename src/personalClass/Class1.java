7539d6bc0a7d62695b8f57b00f604ef311a05923
victorihemadu <victorihemadu@gmail.com>
2025-01-19 03:41:16 +0100

    Basics ofJava

--- src/personalClass/Class1.java
+++ src/personalClass/Class1.java
@@ -1,14 +1,11 @@
 package personalClass;
 
-import java.sql.SQLOutput;
-
 import java.util.Scanner;
-
-import static java.awt.SystemColor.info;
 
 
 public class Class1 {
 
+    /*
     public void anyMethod(){
         System.out.println("First method");
     }
@@ -25,9 +22,12 @@ public class Class1 {
         return 27;
     }
 
+     */
+
     public static void main(String[] args) {
+
         /*
-        String names = "John";
+
         System.out.println(names);
         System.out.println(43&19);
 
@@ -67,20 +67,92 @@ public class Class1 {
 
         System.out.println(STR."my name is \{name} and I am \{age} years old");
         */
+
+        //Loop Class exercise.
         /*
+        # # # #
+        # # # #
+        # # # #
+        # # # #
+
         for(int i=1; i<5; i++) {
             for (int j=0; j<4;  j++) {
                 System.out.print("# ");
             }
             System.out.println(" ");
         }
+
         */
 
-        for(int i=0; i<5; i++) {
-             /*for (int j=i; j<4;  j++) {
+
+
+
+
+        //#
+        //# #
+        //# # #
+        //# # # #
+
+
+/*
+        for(int i=1; i<5; i++) {
+            for (int j=0; j<i;  j++) {
                 System.out.print("# ");
-            }*/
-            System.out.println(" ");
+            }
+            System.out.println("");
         }
+        */
+
+
+
+
+
+
+            //# # # #
+            //# # #
+           // # #
+            //#
+
+        /*
+             for(int i=0; i<5; i++) {
+                for (int j=4; j>i;  j--) {
+                    System.out.print("# ");
+            }
+            System.out.println("");
+        }
+             */
+
+
+
+            //# # # #
+           // #     #
+            //#     #
+            //# # # #
+
+        /*
+        for (int i = 4; i > 0; i--) {
+            for (int j = i; j % i == 1; j--) {
+                System.out.print("# ");
+            }
+        }
+        */
+
+        //System.out.println("#");
+
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        do {
+            System.out.println("Please enter a number between 25 and 35: ");
+            number = scanner.nextInt();
+
+            if (number <=24 || number > 35) {
+                System.out.println("invalid input! Number must be between 25 and 35: ");
+            }
+        } while (number <= 24 || number > 35 );
+
+        System.out.println("Thank you for typing number: " + number);
+        scanner.close();
+
     }
 }
